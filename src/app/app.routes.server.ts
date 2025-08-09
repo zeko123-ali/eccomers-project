41277e108b7c139268c86fd0d05ea7d7ab106d3e
Aboeabd <88734028+Aboeabd@users.noreply.github.com>
2025-08-09 14:27:10 -0500

    add page routes to app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,18 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'Home', renderMode: RenderMode.Server },
+  { path: 'cart', renderMode: RenderMode.Server },
+  { path: 'products', renderMode: RenderMode.Server },
+  { path: 'categories', renderMode: RenderMode.Server },
+  { path: 'brands', renderMode: RenderMode.Server },
+  { path: 'allorders', renderMode: RenderMode.Server },
+  { path: 'checkout/:id', renderMode: RenderMode.Server },
+  { path: 'details/:id', renderMode: RenderMode.Server },
+  { path: 'details2/:id', renderMode: RenderMode.Server },
+  { path: 'login', renderMode: RenderMode.Server },
+  { path: 'register', renderMode: RenderMode.Server },
+  { path: 'forgot', renderMode: RenderMode.Server },
   {
     path: '**',
     renderMode: RenderMode.Prerender
